df1ccb21c8fa877282ee5f1c437a09b9cf0d6f31
Chitrasen <39533707+sonusen235@users.noreply.github.com>
2024-01-04 23:45:16 +0530

    Update PagesController.php

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -16,11 +16,11 @@ class PagesController extends Controller {
 	public function view() {
 		// $user = new User();
 		$user = User::all();
-		echo "<pre>"		;print_r('user');exit;
+		//echo "<pre>"		;print_r('user');exit;
 		// Session::start();
-		Flash::write('This is a test flash message', array('class' => 'required_field'));
-		$flashRead = Flash::read();
-		print_r($flashRead);//exit;
+		//Flash::write('This is a test flash message', array('class' => 'required_field'));
+		//$flashRead = Flash::read();
+		//print_r($flashRead);//exit;
 
 		$sessionId = \lithium\storage\Session::read('session_id');
 		// print_r($sessionId);exit;
